5706fb4a7fa598b80292ef4318bf28de16f29fe4
acesyde <acesyde@gmail.com>
2023-09-04 09:01:22 +0200

    feat: use liters instead of m3

--- custom_components/eau_agur/const.py
+++ custom_components/eau_agur/const.py
@@ -9,10 +9,10 @@ LOGGER: Logger = getLogger(__package__)
 NAME = "EAU par Agur"
 DOMAIN = "eau_agur"
 PLATFORMS = [Platform.SENSOR]
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 COORDINATOR = "coordinator"
 ATTRIBUTION = "Data provided by https://www.agur.fr/"
-SCAN_INTERVAL_IN_MINUTES = 60 * 6  # 6 hours
+SCAN_INTERVAL_IN_MINUTES = 60 * 4  # 4 hours
 
 # Configuration
 CONF_CONTRACT_NUMBER = "contract_number"
